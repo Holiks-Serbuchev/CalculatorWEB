28e2f35cc80d59008181a065296e348d3c6ebb65
Holiks-Serbuchev <dr.serbuchev@mail.ru>
2021-08-31 15:59:44 +0300

    Default

---
+++ CalculatorWEB/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using CalculatorWEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+
+namespace CalculatorWEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        public IActionResult Clicked(double item1, string item2)
+        {
+            double equal = 0;
+            try
+            {
+                if (item2 != "" && item2 != null)
+                {
+                    char sa = item2[item2.Length - 1];
+                    item2 = item2.Remove(item2.Length - 1);
+                    if (sa == '+')
+                        equal = item1 + Convert.ToDouble(item2);
+                    else if (sa == '-')
+                        equal = Convert.ToDouble(item2) - item1;
+                    else if (sa == '*')
+                        equal = item1 * Convert.ToDouble(item2);
+                    else if (sa == '/')
+                        equal = Convert.ToDouble(item2) / item1;
+                }
+            }
+            catch (Exception){}
+            return View("Index", equal);
+        }
+    }
+}
---
+++ CalculatorWEB/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+    function AddValuesTB(clicked_id) {
+            document.getElementById('FirstTB').value += document.getElementById(clicked_id).innerText;
+    }
+    function SelectChar(clicked_id) {
+         if (document.getElementById('FirstTB').value !== "") {
+            document.getElementById('ResultTB').value = document.getElementById('FirstTB').value + document.getElementById(clicked_id).innerText;
+            document.getElementById('FirstTB').value = "";
+         }
+    }
+    function ClearClick() {
+        document.getElementById('ResultTB').value = "";
+        document.getElementById('FirstTB').value = "";
+    }
+    function PointClick(clicked_id) {
+        var numberValue = document.getElementById('FirstTB').value;
+            if (numberValue.indexOf(',') == -1 && numberValue != "")
+                document.getElementById('FirstTB').value += document.getElementById(clicked_id).innerText;
+    }
+
